3bfe6a457e6122e66e5a6341f35bbcc31dfb2dd9
Shinichi TAKATSUKA <pgstaka@gmail.com>
2024-07-05 08:47:26 +0000

    Discordログイン

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    // Discordログイン用
+    'discord' => [
+        'client_id' => env('DISCORD_CLIENT_ID'),
+        'client_secret' => env('DISCORD_CLIENT_SECRET'),
+        'redirect' => env('DISCORD_REDIRECT_URI'),
+    ],
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Auth\LoginController;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Discordログイン用URL
+Route::get('/auth/discord/redirect', function () {
+    return Socialite::driver('discord')->redirect();
+});
+
+// Discordログイン用リダイレクト先
+Route::get('/auth/discord/callback', function () {
+    $socialiteUser = Socialite::driver('discord')->user();
+    return dd($socialiteUser);
 });
